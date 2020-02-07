2ff2531c7a2841421cb282bfafb97d1aa4509d24
rayan-acs <rayan.sadki@protonmail.com>
2020-02-07 09:14:38 +0000

    commit for pull

--- core/Model.php
+++ core/Model.php
@@ -27,8 +27,8 @@ abstract class Model{
             $this->_connexion = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->_connexion->exec("set names utf8");
         }
-        catch(PDOExecption $exception){
-            echo 'Erreur : '. $execption->getMessage();
+        catch(PDOException $exception){
+            echo 'Erreur : '. $exception->getMessage();
         }
     }
 
