407f29ff95c01e5a1d21c461ecf550a94f498ad3
florent <bonneau.florent44@gmail.com>
2020-02-12 15:42:35 +0000

    a

--- models/auth.php
+++ models/auth.php
@@ -1,50 +1,40 @@
 <?php
 
-class auth extends Model{
-  
-    public function __construct(){
-        $this->table ="utilisateur";
+class auth extends Model
+{
+
+    public function __construct()
+    {
+        $this->table = "utilisateur";
         $this->getConnection();
-
-
     }
 
 
 
 
-public function login($name, $passwd){
+    public function login($name, $passwd)
+    {
 
-//verifie que l'user et réel
+        //verifie que l'user et réel
 
 
 
-     $sql="SELECT   count(id) as nb, id, departement_id, nom, prenom, `role` FROM utilisateur WHERE nom=:nom AND `password`= :password";
- 
-    
-    $query = $this->_connexion->prepare($sql);
-    $query->bindValue(':nom', $name, PDO::PARAM_STR);
-    $query->bindValue(':password', $passwd, PDO::PARAM_STR);
-    $query->execute();  
-   $result = $query->fetch(PDO::FETCH_ASSOC);
+        $sql = "SELECT   count(id) as nb, id, departement_id, nom, prenom, `role` FROM utilisateur WHERE nom=:nom AND `password`= :password";
 
-   
-     if($result['nb'] == 0){
-         return false;
+
+        $query = $this->_connexion->prepare($sql);
+        $query->bindValue(':nom', $name, PDO::PARAM_STR);
+        $query->bindValue(':password', $passwd, PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+
+        if ($result['nb'] == 0) {
+            return false;
         }
 
-    if($result['nb'] == 1){
-         return $result;}
-
-
-
-
-        
-
-
-
-}
-
-
-
-
+        if ($result['nb'] == 1) {
+            return $result;
+        }
+    }
 }
